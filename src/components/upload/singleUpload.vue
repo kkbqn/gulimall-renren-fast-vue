599680cb67e29cfb936f57aabb5cf781af906b1a
kkbqn <thj021@126.com>
2024-06-15 23:56:54 +0800

    更改oss请求url

--- src/components/upload/singleUpload.vue
+++ src/components/upload/singleUpload.vue
@@ -1,7 +1,7 @@
 <template> 
   <div>
     <el-upload
-      action="http://gulimall-hello.oss-cn-beijing.aliyuncs.com"
+      action="http://gulimall-kkbqn.oss-cn-hangzhou.aliyuncs.com"
       :data="dataObj"
       list-type="picture"
       :multiple="false" :show-file-list="showFileList"
